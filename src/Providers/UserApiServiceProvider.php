2bffeb0b23428a8ac8c9de1fdf0da3ae392066a9
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-04-12 21:43:24 +0200

    Adding service providers

---
+++ src/Providers/UserApiServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+namespace MercierCorentin\Nextcloud\Providers;
+
+use MercierCorentin\Nextcloud\User;
+use Illuminate\Support\ServiceProvider;
+
+class UserApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('UserApi',function(){
+            return new UserApi();
+        });
+    }
+}
