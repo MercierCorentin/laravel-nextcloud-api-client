e5a09bfc28f31ee499dc2efa661b895d87e9eb75
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-06-12 21:15:51 +0200

    Add GroupApi non-user-dependent tests

--- tests/testGroupApi.php
+++ tests/testGroupApi.php
@@ -2,36 +2,82 @@
 namespace MercierCorentin\Nextcloud\Test;
 
 use MercierCorentin\Nextcloud\Group\GroupApi;
+use MercierCorentin\Nextcloud\Group\Status;
 
-class MyPackageFunctionTest extends TestCase
+class GroupApiTest extends TestCase
 {
+
+    private $groupIdRoot = "Test";
+    
+    private $groupNumber = 5;
+
+    // Group creation tests need to be improved!
+
     /**
-     * Check that the multiply method returns correct result
+     * Check that the groups are created
      * @return void
-     * @dataProvider createGroupProvider
+     * @group creation
+     * @dataProvider groupProvider
      */
     public function testCreateGroup($groupId){
         $response = GroupApi::createGroup($groupId);
+        $this->assertEquals(Status::CREATEGROUP_OK, $response["status"]);
     }
 
-    public function testDeleteGroup(){
+    /**
+     * @return void
+     * @group no_modif
+     * @dataProvider groupProvider
+     */
+    public function testCreateExistGroup($groupId){
+        $response = GroupApi::createGroup($groupId);
+        $this->assertEquals(Status::DELETEGROUP_EXIST, $response["status"]);
     }
 
-    public function testSearchGroups(){
+    /**
+     * @return void
+     * @group no_Modif
+     */
+    public function testSearchGroupsAll(){
+        $response = GroupApi::searchGroups($this->groupIdRoot);
+        $this->assertTrue($response["success"]);
+        $this->assertSame(
+            $this->GroupProvider(),
+            $response["groups"]
+        );
     }
 
+    /**
+     * @return void
+     * @dataProvider groupProvider
+     * @group delete
+     */
+    public function testDeleteGroup($groupId){
+        $response = GroupApi::deleteGroup($groupId);
+        $this->assertTrue($response["success"]);
+    }
+
+
+    /**
+     * @return void
+     * @group no_modif
+     */
     public function testGetGroupUsers(){
 
     }
 
+    /**
+     * @return void
+     * @group no_modif
+     */
     public function testGetGroupSubadmins(){
 
     }
-    // Providers
-    public function createGroupProvider(){
+
+    public function GroupProvider(){
         $groupIds = [];
-        for ($i = 1; $i <= 5; $i++ ){ 
-            array_push($groupIds, "Test".$i );
+        for ($i = 1; $i <= $this->groupNumber; $i++ ){ 
+            array_push($groupIds, $this->groupIdRoot.$i );
         }
         return $groupIds;
     }
