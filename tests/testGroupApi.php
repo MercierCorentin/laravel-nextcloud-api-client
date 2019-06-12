e9d9856b184364023392a56ecb026f7b8c1fa32c
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-06-12 20:25:21 +0200

    Starting GroupApi testing

--- tests/testGroupApi.php
+++ tests/testGroupApi.php
@@ -8,8 +8,10 @@ class MyPackageFunctionTest extends TestCase
     /**
      * Check that the multiply method returns correct result
      * @return void
+     * @dataProvider createGroupProvider
      */
-    public function testCreateGroup(){
+    public function testCreateGroup($groupId){
+        $response = GroupApi::createGroup($groupId);
     }
 
     public function testDeleteGroup(){
@@ -24,5 +26,13 @@ class MyPackageFunctionTest extends TestCase
 
     public function testGetGroupSubadmins(){
 
+    }
+    // Providers
+    public function createGroupProvider(){
+        $groupIds = [];
+        for ($i = 1; $i <= 5; $i++ ){ 
+            array_push($groupIds, "Test".$i );
+        }
+        return $groupIds;
     }
 }
