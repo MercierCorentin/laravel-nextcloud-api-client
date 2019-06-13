27b564f5cb9f316ba2ccbf7cb55f96fa096df776
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-06-13 23:05:44 +0200

    Remove hardcoded data

--- config/nextcloud.php
+++ config/nextcloud.php
@@ -4,6 +4,6 @@
         "nextcloud" => [
             'login'     => env('NEXTCLOUD_LOGIN', 'admin'),
             'password'  => env('NEXTCLOUD_PASSWORD', 'password'),
-            'baseUrl'   => env('NEXTCLOUD_BASEURL', 'https://cloud.test.picasoft.net'),
+            'baseUrl'   => env('NEXTCLOUD_BASEURL', ''),
         ]
     ];
